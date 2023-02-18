872f85825b360b2c59afafc981a888027cfcf375
infected_ <zzitoz@gmail.com>
2023-02-18 10:31:31 +0000

    defconfig: define hostname

--- fast.sh
+++ fast.sh
@@ -3,17 +3,17 @@ export KERNEL_DIR="/home/deepongi/Android/Singularity_Kernel"
 export KBUILD_OUTPUT="/home/deepongi/Android/Singularity_Kernel/out"
 export ZIP_DIR="/home/deepongi/Android/AnyKernel3"
 export ZIP_OUT_DIR="/home/deepongi/Android/Out_Zips"
-#make O=out mrproper
-#rm -rf out
+make O=out mrproper
+rm -rf out
 export PATH="/mnt/Hawai/toolchains/proton-clang-16/bin:$PATH"
 export USE_CCACHE=1
 export ARCH=arm64
-export VARIANT="singularity-r07"
+export VARIANT="singularity-r08"
 export HASH=`git rev-parse --short=4 HEAD`
 export KERNEL_ZIP="$VARIANT-$HASH"
-export LOCALVERSION="~singularity-r07"
+export LOCALVERSION="~singularity-r08"
 make O=$KBUILD_OUTPUT CC=clang  LLVM=1 LLVM_IAS=1 evo_defconfig
-make -j64 O=$KBUILD_OUTPUT CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 CROSS_COMPILE=/mnt/Hawai/toolchains/gcc-linaro-12.2.1-2023.01-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=/mnt/Hawai/toolchains/gcc-linaro-12.2.1-2023.01_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+make -j64 O=$KBUILD_OUTPUT CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 CROSS_COMPILE=/mnt/Hawai/toolchains/gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=/mnt/Hawai/toolchains/gcc-linaro-13.0.0-2022.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 cp -v $KBUILD_OUTPUT/arch/arm64/boot/Image $ZIP_DIR/Image
 cd $ZIP_DIR
 zip -r9 $VARIANT-$HASH.zip *
