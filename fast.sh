a1f860e0b8ac7bffc61deb2ca57f1d323a2f54a7
infected_ <zzitoz@gmail.com>
2023-02-19 22:16:35 +0000

    script: update

--- fast.sh
+++ fast.sh
@@ -8,10 +8,10 @@ export ZIP_OUT_DIR="/home/deepongi/Android/Out_Zips"
 export PATH="/mnt/Hawai/toolchains/proton-clang-16/bin:$PATH"
 export USE_CCACHE=1
 export ARCH=arm64
-export VARIANT="singularity-r08"
+export VARIANT="singularity-r09"
 export HASH=`git rev-parse --short=4 HEAD`
 export KERNEL_ZIP="$VARIANT-$HASH"
-export LOCALVERSION="~singularity-r08"
+export LOCALVERSION="~singularity-r09"
 make O=$KBUILD_OUTPUT CC=clang  LLVM=1 LLVM_IAS=1 evo_defconfig
 make -j64 O=$KBUILD_OUTPUT CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LLVM=1 LLVM_IAS=1 CROSS_COMPILE=/mnt/Hawai/toolchains/gcc-linaro-13.0.0-2022.11-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu- CROSS_COMPILE_ARM32=/mnt/Hawai/toolchains/gcc-linaro-13.0.0-2022.11-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 cp -v $KBUILD_OUTPUT/arch/arm64/boot/Image $ZIP_DIR/Image
